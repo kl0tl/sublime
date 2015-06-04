af5edbe03d8defbf9cba5b172e966c32c7b474cb
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2015-06-04 17:42:13 +0200

    fix: make `move_to_quotes` more similar to the `move_to` command

--- MoveToQuotesCommand.py
+++ MoveToQuotesCommand.py
@@ -1,55 +1,98 @@
 import sublime, sublime_plugin
 
-# Test cases:
-#
-# `view.run_command("move_to_quotes", { "expand": True })`
-#   + With cursor at X, the command should select the string:
-#       "Here is the X cursor"
-#
-#   + With cursor at X, the command should select the single quoted string:
-#       "Here is 'the X cursor' now"
-#
-#   + With cursor at X, the command should select the double quoted string:
-#       "Here the cursor is 'outside' the X selection"
-#
-# Without `expand` the command should move the cursor alternatively before the last and after the first quotes of the selected region
-
 class MoveToQuotesCommand(sublime_plugin.TextCommand):
   def run(self, edit, expand = False):
-    print(expand)
-
-    d_quotes = list(map(lambda x: x.begin(), self.view.find_all('"')))
-    s_quotes = list(map(lambda x: x.begin(), self.view.find_all("'")))
-
     for sel in self.view.sel():
-      def search_for_quotes(q_type, quotes):
-        q_size, before, after = False, False, False
+      left_parser = Parser(self.view, sel.begin())
+      right_parser = Parser(self.view, sel.end())
 
-        if len(quotes) - self.view.substr(sel).count('"') >= 2:
-          all_before = list(filter(lambda x: x < sel.begin(), quotes))
-          all_after = list(filter(lambda x: x >= sel.end(), quotes))
+      if expand and left_parser.is_after_begin_quote() and right_parser.is_before_end_quote():
+        self.move_cursor(sel, left_parser.move_before_begin_quote(), right_parser.move_after_end_quote())
+      elif not (expand and left_parser.is_before_begin_quote() and right_parser.is_after_end_quote()):
+        if sel.b < sel.a:
+          left_parser, right_parser = right_parser, left_parser
 
-          if all_before: before = all_before[-1]
-          if all_after: after = all_after[0]
+        if right_parser.is_before_begin_quote():
+          right_parser.move_after_end_quote()
+        elif right_parser.is_after_begin_quote():
+          right_parser.move_before_end_quote()
+        elif right_parser.is_before_end_quote():
+          right_parser.move_after_begin_quote()
+        elif right_parser.is_after_end_quote():
+          right_parser.move_before_begin_quote()
+        elif right_parser.is_quoted():
+          left_parser.move_after_begin_quote()
+          right_parser.move_before_end_quote()
 
-          if all_before and all_after: q_size = after - before
+        start = left_parser.pos
+        end = right_parser.pos
 
-        return q_size, before, after
+        self.move_cursor(sel, start if expand else end, end)
 
-      d_size, d_before, d_after = search_for_quotes('"', d_quotes)
-      s_size, s_before, s_after = search_for_quotes("'", s_quotes)
+  def move_cursor(self, cursor, start, end):
+    regions = self.view.sel()
+    regions.subtract(cursor)
+    regions.add(sublime.Region(start, end))
 
-      def replace_region(start, end):
-        if sel.size() < end-start-2:
-          start += 1; end -= 1
-        self.view.sel().subtract(sel)
-        if expand:
-          self.view.sel().add(sublime.Region(start, end))
-        else:
-          pos = start if sel.end() == end else end
-          self.view.sel().add(sublime.Region(pos, pos))
+class Parser:
+  def __init__(self, view, pos = 0):
+    self.view = view
+    self.pos = pos
+    self.begin_quote_scope = 'punctuation.definition.string.begin'
+    self.end_quote_scope = 'punctuation.definition.string.end'
+    self.quoted_scope = 'string.quoted'
 
-      if d_size and (not s_size or d_size < s_size):
-        replace_region(d_before, d_after+1)
-      elif s_size and (not d_size or s_size < d_size):
-        replace_region(s_before, s_after+1)
+  def is_before_begin_quote(self):
+    return self.matches_scope(self.begin_quote_scope)
+
+  def is_after_begin_quote(self):
+    return self.matches_scope_at(self.pos - 1, self.begin_quote_scope)
+
+  def is_before_end_quote(self):
+    return self.matches_scope(self.end_quote_scope)
+
+  def is_after_end_quote(self):
+    return self.matches_scope_at(self.pos - 1, self.end_quote_scope)
+
+  def is_quoted(self):
+    return self.matches_scope(self.quoted_scope)
+
+  def matches_scope(self, scope):
+    return self.matches_scope_at(self.pos, scope)
+
+  def matches_scope_at(self, pos, scope):
+    return self.view.score_selector(pos, scope) > 0
+
+  def move_before_end_quote(self):
+    return self.move(self.next, self.is_before_end_quote)
+
+  def move_after_end_quote(self):
+    return self.move(self.next, self.is_after_end_quote)
+
+  def move_before_begin_quote(self):
+    return self.move(self.prev, self.is_before_begin_quote)
+
+  def move_after_begin_quote(self):
+    return self.move(self.prev, self.is_after_begin_quote)
+
+  def move(self, step, predicate):
+    while True:
+      if predicate():
+        break
+      else:
+        step()
+    return self.pos
+
+  def prev(self):
+    if self.pos > 0:
+      self.pos -= 1
+      return True
+    else:
+      return False
+
+  def next(self):
+    if self.pos < self.view.size():
+      self.pos += 1
+      return True
+    else:
+      return False
