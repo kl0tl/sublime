9ed330941f09db01c0a310d70c055c153c0c8bf6
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2015-06-14 20:11:51 +0200

    feat: readd SideBarEnhancements for all the `side_bar_X` commands

--- CopyFileNameCommand.py
+++
@@ -1,8 +0,0 @@
-import os, sublime, sublime_plugin
-
-class CopyFileNameCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    file_name = self.view.file_name()
-    if file_name:
-      base_name = os.path.basename(file_name)
-      sublime.set_clipboard(base_name)
