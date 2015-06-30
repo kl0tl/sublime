aa82d0360146bf9fc5b112729c0df95d714de0cb
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2015-06-30 21:20:41 +0200

    feat: ask for how many times to repeat when not specified

--- RepeatLastCommandCommand.py
+++ RepeatLastCommandCommand.py
@@ -1,7 +1,26 @@
 import sublime, sublime_plugin
 
 class RepeatLastCommandCommand(sublime_plugin.TextCommand):
-  def run(self, edit, times = 1):
+  def run(self, edit, times = None):
+    if times == None:
+      self.ask_for_ttr()
+    else:
+      self.repeat_last_command(times)
+
+  def on_done(self, times):
+    try:
+      ttr = int(times)
+    except ValueError:
+      message = str.format("Can’t repeat the last command ‘{0}’ times.\n\nPlease insert a number.", times)
+      if sublime.ok_cancel_dialog(message):
+        self.ask_for_ttr()
+      return
+    self.repeat_last_command(ttr)
+
+  def ask_for_ttr(self):
+    self.view.window().show_input_panel('How many times ?', '', self.on_done, None, None)
+
+  def repeat_last_command(self, times):
     command, args, _ = self.view.command_history(0, True)
     self.schedule_command(command, args, times)
 
