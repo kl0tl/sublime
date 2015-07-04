40be3c8d7bc2aacffcd3ef28f36a3642240c58cc
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2015-07-04 19:49:12 +0200

    chore: make CycleGroup a WindowCommand

--- CycleGroup.py
+++ CycleGroup.py
@@ -1,10 +1,9 @@
 import sublime_plugin
 
-
-class CycleGroupCommand(sublime_plugin.TextCommand):
-  def run(self, edit, forward=True):
-    num_groups = self.view.window().num_groups() - 1
-    active_group = self.view.window().active_group()
+class CycleGroupCommand(sublime_plugin.WindowCommand):
+  def run(self, forward=True):
+    num_groups = self.window.num_groups() - 1
+    active_group = self.window.active_group()
     next_group = active_group + (1 if forward else -1)
 
     if next_group > num_groups:
@@ -12,4 +11,4 @@ class CycleGroupCommand(sublime_plugin.TextCommand):
     elif (next_group < 0):
       next_group = num_groups
 
-    self.view.window().focus_group(next_group)
+    self.window.focus_group(next_group)
